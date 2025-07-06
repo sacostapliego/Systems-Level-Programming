361096098b5842cda601337363326c45aabab6ad
Steven <steven.acosta867@gmail.com>
2025-07-05 23:50:31 -0400

    small cosmetic change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './page.module.css'; // Assuming you have some styles
+import styles from './page.module.css';
 
 export default function Home() {
   const programs = [
